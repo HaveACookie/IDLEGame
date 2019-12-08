968735409fab785516b1ba57644d3537f7d0327f
Christian Weinschenk <cbw307@nyu.edu>
2019-12-08 14:07:51 -0500

    Moving Project into Repo

---
+++ IdleTest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    private static GameManager _instance;
+    public static GameManager Instance
+
+    {
+        get
+        {
+            //Checks to see if manager exists and creates an instance if it doesn't exist
+            if (_instance == null)
+            {
+                GameObject go = new GameObject("GameManager");
+                go.AddComponent<GameManager>();
+
+
+
+            }
+            return _instance;
+        }
+
+    }
+
+    //Uses Accessors to help implement global accessibility for important variables that must be acccessed accross multiple objects/scripts
+    public int Money { get; set; }
+    public int Farms { get; set; }
+    public int Burgers { get; set; }
+    public int Stores { get; set; }
+    public int Trucks { get; set; }
+    public double FarmPrice { get; set; }
+    public double StorePrice { get; set; }
+    public double TruckPrice { get; set; }
+
+    public int baseRate { get; set;}
+    
+
+    public int MaximumOutput { get; set; }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+
+    }
+}
+
---
+++ IdleTest/Assets/Scripts/MoneySTuff.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneySTuff : MonoBehaviour
+{
+
+    [SerializeField]
+    private int rawProduction;
+    private int realProduction;
+    private int maxCanTransport;
+    private int maxCanSell;
+    public int sold;
+    public int selling;
+    private int surplus;
+
+    public Text moneyText;
+    public Text soldText;
+    public Text farmText;
+    public Text truckText;
+    public Text storeText;
+    public Text cookText;
+    public Text sellingText;
+    public Text rawProductionText;
+    public Text realProductionText;
+    public Text surplusText;
+
+      
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.Instance.FarmPrice = 10;
+        GameManager.Instance.TruckPrice = 15;
+        GameManager.Instance.StorePrice = 44;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moneyText.text  = GameManager.Instance.Money + " " + "Dollars";
+        soldText.text = sold + "Sandwhiches Sold";
+        farmText.text = GameManager.Instance.Farms + "Farms " + "Buy Another for" + " " + GameManager.Instance.FarmPrice;
+        storeText.text = GameManager.Instance.Stores + "Stores " + "Buy Another for" + " " + GameManager.Instance.StorePrice;
+        truckText.text = GameManager.Instance.Trucks + "Trucks" + "Buy Another for" + " " + GameManager.Instance.TruckPrice;
+        cookText.text = "Cook/Sell 1 Burger at home ";
+        sellingText.text = "Selling " + selling + " per second";
+        rawProductionText.text = "raw Production " + rawProduction;
+        realProductionText.text = "real Production  " + realProduction;
+        surplusText.text = "Surplus " + surplus;
+
+ 
+        rawProduction += (GameManager.Instance.Farms * 10);
+        maxCanTransport = (GameManager.Instance.Trucks * 100);
+        maxCanSell = (GameManager.Instance.Stores * 200);
+
+        if (rawProduction > maxCanTransport)
+        {
+            surplus += rawProduction - maxCanTransport;
+            realProduction = maxCanTransport;
+            
+        }
+        if(rawProduction <= maxCanTransport)
+        {
+            realProduction = rawProduction;
+        }
+        if(maxCanTransport > maxCanSell)
+        {
+            surplus += maxCanSell - maxCanTransport;
+            //sold += maxCanSell;
+            selling += maxCanSell;
+        }
+        if(maxCanTransport <= maxCanSell)
+        {
+            //sold += maxCanTransport;
+            selling += maxCanTransport;
+        }
+
+        
+        GameManager.Instance.Money += selling * 5;
+
+        sold += selling;
+
+    }
+
+     public void farmButton()
+    {
+        if (GameManager.Instance.Money >= GameManager.Instance.FarmPrice)
+        {
+            GameManager.Instance.Farms += 1;
+            GameManager.Instance.FarmPrice = (10 * (10^GameManager.Instance.Farms));
+        }
+        
+    }
+    public void truckButton()
+    {
+        if (GameManager.Instance.Money >= GameManager.Instance.TruckPrice)
+        {
+            GameManager.Instance.Trucks += 1;
+            GameManager.Instance.TruckPrice = (GameManager.Instance.TruckPrice * 1.1);
+        }
+
+    }
+    public void storeButton()
+    {
+        if(GameManager.Instance.Money>= GameManager.Instance.StorePrice)
+        {
+            GameManager.Instance.Stores += 1;
+            GameManager.Instance.StorePrice = (GameManager.Instance.StorePrice * 1.1);
+        }
+    }
+
+    public void cookButton()
+    {
+        sold++;
+        GameManager.Instance.Money += 5;
+    }
+
+}
